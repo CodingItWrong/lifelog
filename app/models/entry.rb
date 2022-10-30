50ef4502a283b390319f0b71249c5d456116142e
Josh Justice <me@codingitwrong.com>
2022-10-30 13:52:56 -0400

    Set time upon initialize instead of create

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class Entry < ApplicationRecord
-  before_create :set_log_time
+  after_initialize :set_log_time
 
   def set_log_time
     self.log_time = Time.now
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
   include ActiveSupport::Testing::TimeHelpers
-  
-  describe 'creating' do
+
+  describe 'initializing' do
     it 'sets the log_time to the current time' do
       freeze_time do
-        entry = Entry.create!
+        entry = Entry.new
         expect(entry.log_time).to eq(Time.now)
       end
     end
